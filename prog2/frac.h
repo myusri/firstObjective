37d5ca0d9672a0a04f4c979f8f4550764b1ae562
Mohamad Yusri <myusri@gmail.com>
2017-12-04 15:08:44 +0800

    explicit setters/getters

---
+++ prog2/frac.h
@@ -0,0 +1,13 @@
+//
+//  frac.h
+//  prog2
+//
+//  Created by Mohamad Yusri on 04/12/2017.
+//  Copyright © 2017 Mohamad Yusri. All rights reserved.
+//
+
+#ifndef frac_h
+#define frac_h
+
+
+#endif /* frac_h */
---
+++ prog2/frac.m
@@ -0,0 +1,9 @@
+//
+//  frac.m
+//  prog2
+//
+//  Created by Mohamad Yusri on 04/12/2017.
+//  Copyright © 2017 Mohamad Yusri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
