188ea16aa421cc18a966d93fcff7d34896535cfe
Mohamad Yusri <myusri@gmail.com>
2017-12-04 13:49:38 +0800

    Initial Commit

---
+++ prog2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog2
+//
+//  Created by Mohamad Yusri on 04/12/2017.
+//  Copyright © 2017 Mohamad Yusri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
